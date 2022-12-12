82ab55d713b38cc470e02a4109c3985296fced9e
Kolombo <h.kolombooo@gmail.com>
2022-12-12 11:22:49 +0100

    Initial commit

---
+++ src/main/java/me/kolombo/eventer/Eventer.java
@@ -0,0 +1,17 @@
+package me.kolombo.eventer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Eventer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
