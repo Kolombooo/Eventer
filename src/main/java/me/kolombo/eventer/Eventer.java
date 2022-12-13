a1ab6a107bfab4a4817dcb56166d3ab9013e4005
Kolombo <h.kolombooo@gmail.com>
2022-12-13 13:10:30 +0100

    First config

--- src/main/java/me/kolombo/eventer/Eventer.java
+++ src/main/java/me/kolombo/eventer/Eventer.java
@@ -1,17 +1,28 @@
 package me.kolombo.eventer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class Eventer extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        boolean DebugMode = getConfig().getBoolean("debug");
+        String ConfigVersion = getConfig().getString("config-version");
+
+        Bukkit.getLogger().log(Level.INFO, "Eventer plugin enabled. Welcome!");
+        System.out.println("Debug mode: " + DebugMode);
+        System.out.println("Config version: " + ConfigVersion);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        System.out.println("");
+
     }
 }
